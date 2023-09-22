7a0c63bed257c05dbeb6c4fa4f9b9d704f6ecfa2
Zorin-Mykhailo <zorin.mikhail.84284@gmail.com>
2023-09-22 19:05:44 +0300

    Add project files.

---
+++ Building/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text;
+
+namespace Building;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.InputEncoding = Encoding.Unicode;
+        Console.OutputEncoding = Encoding.Unicode;
+        Console.WriteLine("✨ Neutrino_Building");
+    }
+}
