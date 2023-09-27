ec10613f7c8c2cfb52646aac75acbeaea1dc38d1
Hordii Moroz <gordya1564@gmail.com>
2023-09-27 13:05:58 +0300

    1 commit

---
+++ Building/Company.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Building
+{
+    public class Company
+    {
+        // Назва команії, направлення у галузі (воно не змінюється), опис
+        private string _nameOfCompany;
+        private const string _directionInIndustry = "Construction and restoration";
+        readonly string _description;
+
+        public required string nameOfCompany
+        {
+            get
+            {
+                return _nameOfCompany;
+            }
+            set
+            {
+                _nameOfCompany = value;
+            }
+        }
+
+    }
+}
