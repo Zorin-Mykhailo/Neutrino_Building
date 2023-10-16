aea222bd82b9616442849ec3ea64c34933bd0c6a
Sasai Kudasaiiii <saintcavvy@gmail.com>
2023-10-16 03:57:34 +0300

    Added record type

---
+++ Building/DataModel/Prices/DateCounter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Building;
+internal record DateCounter 
+{
+    public DateTime Date { get; init; }
+
+    public DateCounter ()
+    {
+        DateTime Today = DateTime.Now;
+        Date = Today.AddDays(2);   
+    }
+}
